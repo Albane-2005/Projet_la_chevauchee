a1146e0bb90ef52c62026acb26bfa92017ce00ee
Escudero <albane.escudero@epfedu.fr>
2026-01-03 18:44:56 +0100

    nouvelle classe case

--- Projet_la_chevauchee/Projet_la_chevauchee/src/Case.java
+++ Projet_la_chevauchee/Projet_la_chevauchee/src/Case.java
@@ -8,5 +8,27 @@
  * @author alban
  */
 public class Case {
+    private final int ligne;
+    private final int colonne;
     
+    public Case(int ligne, int colonne) {
+        this.ligne = ligne;
+        this.colonne = colonne;
+    }   
+    
+    public int getLigne() {
+        return ligne;
+    }
+
+    public int getColonne() {
+        return colonne;
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Case aCase = (Case) o;
+        return ligne == aCase.ligne && colonne == aCase.colonne;
+    }
 }
